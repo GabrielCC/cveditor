33c3396191d49b75460dc72ea350d9ddbe269c71
Gabriel Croitoru <gabriel.croitoru.cristian@gmail.com>
2013-09-26 22:38:48 +0300

    fixed migration files

--- db/migrate/20130924164255_create_phones.rb
+++ db/migrate/20130924164255_create_phones.rb
@@ -1,7 +1,7 @@
 class CreatePhones < ActiveRecord::Migration
   def change
     create_table :phones do |t|
-      t.string, :type
+      t.string :type
       t.string :number
 
       t.timestamps
--- db/migrate/20130924164301_create_instant_messagings.rb
+++ db/migrate/20130924164301_create_instant_messagings.rb
@@ -1,7 +1,7 @@
 class CreateInstantMessagings < ActiveRecord::Migration
   def change
     create_table :instant_messagings do |t|
-      t.string, :type
+      t.string :type
       t.string :address
 
       t.timestamps
